9fa59a563c679e329193ba3d3164d8a4892253be
Shieldine <74987363+Shieldine@users.noreply.github.com>
2025-12-01 16:02:59 +0100

    reformat

--- day_1.py
+++ day_1.py
@@ -9,9 +9,9 @@ with open("./input/day_1.txt") as f:
     for line in f:
         change = 0
         if "R" in line:
-            change = int(line.replace("R","").strip())
+            change = int(line.replace("R", "").strip())
         if "L" in line:
-            change = -int(line.replace("L","").strip())
+            change = -int(line.replace("L", "").strip())
 
         rots = abs(change) // MAX
         count_2 += rots
@@ -20,14 +20,13 @@ with open("./input/day_1.txt") as f:
 
         if change < 0 < cur and cur + change <= 0:
             count_2 += 1
-        elif change > 0 and cur + change > MAX-1:
+        elif change > 0 and cur + change > MAX - 1:
             count_2 += 1
 
         cur = (cur + change) % MAX
 
         if cur == 0:
             count_1 += 1
-
 
 print(count_1)
 print(count_2)
