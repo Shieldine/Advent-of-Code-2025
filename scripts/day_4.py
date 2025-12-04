90ab7a504ca6d64502a41e6ea0b5def84833a0c1
Shieldine <74987363+Shieldine@users.noreply.github.com>
2025-12-04 10:27:40 +0100

    solve day 4 part 2

--- scripts/day_4.py
+++ scripts/day_4.py
@@ -1,5 +1,3 @@
-accessible = 0
-
 DIRECTIONS = [
     [1, 0],
     [1, 1],
@@ -16,19 +14,36 @@ with open("../input/day_4.txt") as f:
 
 m = [list(l.strip()) for l in m]
 
-for i in range(len(m)):
-    for j in range(len(m[i])):
-        if m[i][j] == ".":
-            continue
-        count = 0
-        for direction in DIRECTIONS:
-            ni = i + direction[0]
-            nj = j + direction[1]
-            if 0 <= ni < len(m) and 0 <= nj < len(m[ni]):
-                if m[i + direction[0]][j + direction[1]] != ".":
-                    count += 1
 
-        if count < 4:
-            accessible += 1
+def remove_rolls(roll_map):
+    to_remove = []
+    for i in range(len(roll_map)):
+        for j in range(len(roll_map[i])):
+            if roll_map[i][j] == ".":
+                continue
+            count = 0
+            for direction in DIRECTIONS:
+                ni = i + direction[0]
+                nj = j + direction[1]
+                if 0 <= ni < len(m) and 0 <= nj < len(m[ni]):
+                    if roll_map[i + direction[0]][j + direction[1]] != ".":
+                        count += 1
 
-print(accessible)
+            if count < 4:
+                to_remove.append((i, j))
+
+    for pair in to_remove:
+        roll_map[pair[0]][pair[1]] = "."
+
+    return len(to_remove)
+
+
+removed = remove_rolls(m)
+total = removed
+print(removed)
+
+while removed > 0:
+    removed = remove_rolls(m)
+    total += removed
+
+print(total)
