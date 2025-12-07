48ce0852743cb4804331571b82b6602b35e1b976
Shieldine <74987363+Shieldine@users.noreply.github.com>
2025-12-07 11:51:54 +0100

    refactor, solve part 2

--- scripts/day_6.py
+++ scripts/day_6.py
@@ -2,9 +2,14 @@ import math
 
 nums = []
 operands = []
+raw = []
 
 with open("../input/day_6.txt") as f:
     for line in f:
+        line = line.replace("\n", "")
+        line += " "
+        raw.append(list(line))
+
         if "+" in line or "*" in line:
             operands = list(line.strip().replace(" ", ""))
         else:
@@ -19,5 +24,34 @@ for idx, op in enumerate(operands):
         total += sum([num[idx] for num in nums])
     else:
         total += math.prod([num[idx] for num in nums])
+
+print(total)
+
+flipped = []
+
+for i in range(len(raw[0])):
+    flipped.append([])
+    for j in range(len(raw)):
+        flipped[i].append(raw[j][i])
+
+nums = []
+operand = ""
+
+total = 0
+
+for col in flipped:
+    if all('' == s or s.isspace() for s in col):
+        if operand == "*":
+            total += math.prod(nums)
+        elif operand == "+":
+            total += sum(nums)
+        nums = []
+        operand = ""
+        continue
+    if "*" not in col and "+" not in col:
+        nums.append(int(''.join(col)))
+    else:
+        nums.append(int(''.join(col[:-1])))
+        operand = col[-1]
 
 print(total)
