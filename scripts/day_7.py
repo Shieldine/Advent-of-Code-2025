ff645996c09f439242dff9ac88b22a6603e8236f
Shieldine <74987363+Shieldine@users.noreply.github.com>
2025-12-07 11:18:10 +0100

    refactor, solve part 2

--- scripts/day_7.py
+++ scripts/day_7.py
@@ -1,17 +1,12 @@
-beams = set()
+import copy
+
 diagram = []
-
-
-def print_formatted(d):
-    for r in d:
-        print(" ".join(r))
-
 
 with open("../input/day_7.txt") as f:
     for line in f:
         diagram.append(list(line.strip()))
 
-beams.add((0, diagram[0].index("S")))
+start = (0, diagram[0].index("S"))
 
 
 def traverse(d, b):
@@ -19,20 +14,43 @@ def traverse(d, b):
     new_beams = set()
 
     for beam in b:
-        if beam[0] + 1 > len(diagram) - 1:
+        if beam[0] + 1 > len(d) - 1:
             continue
-        if diagram[beam[0] + 1][beam[1]] == ".":
+        if d[beam[0] + 1][beam[1]] == ".":
             new_beams.add((beam[0] + 1, beam[1]))
-            diagram[beam[0] + 1][beam[1]] = "|"
-        elif diagram[beam[0] + 1][beam[1]] == "^":
+            d[beam[0] + 1][beam[1]] = "|"
+        elif d[beam[0] + 1][beam[1]] == "^":
             new_beams.add((beam[0], beam[1] - 1))
             new_beams.add((beam[0], beam[1] + 1))
             splitters.add((beam[0] + 1, beam[1]))
 
     if len(new_beams) == 0:
-        return splitters
+        return len(splitters)
 
-    return splitters | traverse(d, new_beams)
+    return len(splitters) + traverse(d, new_beams)
 
 
-print(len(traverse(diagram, beams)))
+print(traverse(copy.deepcopy(diagram), {start}))
+
+path_cache = {}
+
+
+def traverse_single(d, beam):
+    paths = 0
+
+    if beam in path_cache:
+        return path_cache[beam]
+
+    if beam[0] + 1 > len(d) - 1:
+        return 1
+    if d[beam[0] + 1][beam[1]] == ".":
+        result = traverse_single(d, (beam[0] + 1, beam[1]))
+        path_cache[beam] = paths + result
+        return paths + result
+    elif d[beam[0] + 1][beam[1]] == "^":
+        result = traverse_single(d, (beam[0], beam[1] - 1)) + traverse_single(d, (beam[0], beam[1] + 1))
+        path_cache[beam] = paths + result
+        return paths + result
+
+
+print(traverse_single(diagram, start))
