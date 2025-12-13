68faaf069a56fd9045b96ea351e1cd05c7c39294
Shieldine <74987363+Shieldine@users.noreply.github.com>
2025-12-13 23:18:41 +0100

    solve day 11 part 2

--- scripts/day_11.py
+++ scripts/day_11.py
@@ -1,7 +1,5 @@
 devices = {}
 
-paths = 0
-
 with open("../input/day_11.txt") as f:
     for line in f.readlines():
         line = line.strip()
@@ -23,4 +21,33 @@ def traverse(current):
     return cur
 
 
+cache = {}
+
+
+def find_paths(current, need_dac, need_fft):
+    cache_key = (current, need_dac, need_fft)
+    if cache_key in cache:
+        return cache[cache_key]
+
+    if current == "out":
+        return 1 if (not need_dac and not need_fft) else 0
+
+    still_need_dac = need_dac and (current != "dac")
+    still_need_fft = need_fft and (current != "fft")
+
+    num = 0
+    for d in devices[current]:
+        num += find_paths(d, still_need_dac, still_need_fft)
+
+    cache[cache_key] = num
+    return num
+
+
 print(traverse("you"))
+
+count = 0
+
+for d in devices["svr"]:
+    count += find_paths(d, True, True)
+
+print(count)
