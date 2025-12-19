79a089f47c9ea947aea092d557820e8e356d819a
Shieldine <74987363+Shieldine@users.noreply.github.com>
2025-12-19 18:26:10 +0100

    solve day 9 part 2

--- scripts/day_9.py
+++ scripts/day_9.py
@@ -1,7 +1,7 @@
 import itertools
 
 import shapely
-from shapely import Point
+from shapely.prepared import prep
 
 coords = []
 
@@ -12,6 +12,7 @@ with open("../input/day_9.txt") as f:
 
 combs = itertools.combinations(coords, 2)
 polygon = shapely.geometry.Polygon([(x, -y) for x, y in coords])
+prepared_polygon = prep(polygon)
 
 largest = 0
 second_largest = 0
@@ -22,24 +23,19 @@ for comb in combs:
     if area > largest:
         largest = area
 
-    tiled = 0
+    if area < second_largest:
+        continue
+
     x1, y1 = comb[0]
     x2, y2 = comb[1]
 
-    all_inside = True
-    for x in range(min(x1, x2), max(x1, x2) + 1):
-        for y in range(min(y1, y2), max(y1, y2) + 1):
-            if polygon.covers(Point(x, -y)):
-                tiled += 1
-            else:
-                all_inside = False
-                break
+    min_x, max_x = min(x1, x2), max(x1, x2)
+    min_y, max_y = min(y1, y2), max(y1, y2)
 
-    if not all_inside:
-        continue
+    rectangle = shapely.geometry.box(min_x, -max_y, max_x, -min_y)
 
-    if tiled > second_largest:
-        second_largest = tiled
+    if prepared_polygon.covers(rectangle):
+        second_largest = area
 
 print(largest)
 print(second_largest)
