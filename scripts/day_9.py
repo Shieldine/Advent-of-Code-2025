ed16366ef1741d66cad1e1ba7a361cfa58c3ab94
Shieldine <74987363+Shieldine@users.noreply.github.com>
2025-12-19 18:05:29 +0100

    wip on day 9 part 2

--- scripts/day_9.py
+++ scripts/day_9.py
@@ -1,5 +1,8 @@
 import itertools
 
+import shapely
+from shapely import Point
+
 coords = []
 
 with open("../input/day_9.txt") as f:
@@ -8,8 +11,10 @@ with open("../input/day_9.txt") as f:
         coords.append((int(first), int(second)))
 
 combs = itertools.combinations(coords, 2)
+polygon = shapely.geometry.Polygon([(x, -y) for x, y in coords])
 
 largest = 0
+second_largest = 0
 
 for comb in combs:
     area = (abs(comb[0][0] - comb[1][0]) + 1) * (abs(comb[0][1] - comb[1][1]) + 1)
@@ -17,4 +22,24 @@ for comb in combs:
     if area > largest:
         largest = area
 
+    tiled = 0
+    x1, y1 = comb[0]
+    x2, y2 = comb[1]
+
+    all_inside = True
+    for x in range(min(x1, x2), max(x1, x2) + 1):
+        for y in range(min(y1, y2), max(y1, y2) + 1):
+            if polygon.covers(Point(x, -y)):
+                tiled += 1
+            else:
+                all_inside = False
+                break
+
+    if not all_inside:
+        continue
+
+    if tiled > second_largest:
+        second_largest = tiled
+
 print(largest)
+print(second_largest)
